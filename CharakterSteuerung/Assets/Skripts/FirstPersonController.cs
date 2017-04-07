a10bb22654e30d50c45bf893a6d6b372bd24272c
Jasmin <jasmin.leefers@web.de>
2017-04-07 09:33:42 +0200

    TestProjekt

---
+++ CharakterSteuerung/Assets/Skripts/FirstPersonController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+    public float movementspeed = 8.0f;
+    public float mousespeed = 5.0f;
+    public float updownRange = 40.0f;
+    float verticalRotation = 0;
+
+	// Use this for initialization
+	void Start () {
+        Screen.lockCursor = (true);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Rotation
+        float siderotate = Input.GetAxis("Mouse X") * mousespeed;
+        verticalRotation -= Input.GetAxis("Mouse Y") * mousespeed;
+
+        float currentupdown = Camera.main.transform.rotation.eulerAngles.x;
+
+        verticalRotation = Mathf.Clamp(verticalRotation, -updownRange, updownRange);
+        //Blickrichtung nach oben und unten wird nur durch neigung der Camera relaisiert
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        
+
+        //Blickrichtung zur Seite dreht das ganze Spielermodell
+        transform.Rotate(0, siderotate, 0);
+
+        //Bewegung
+        float forwardSpeed = Input.GetAxis("Vertical")* movementspeed;
+        float sidespeed = Input.GetAxis("Horizontal") * movementspeed;
+        Vector3 speed = new Vector3(sidespeed,0 , forwardSpeed);
+
+        speed = transform.rotation * speed; //Vector an Playerachsen ausrichten
+        CharacterController cc = GetComponent<CharacterController>();
+        cc.SimpleMove(speed);
+	
+	}
+}
---
+++ CharakterSteuerung/Assets/Skripts/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    public GameObject[] rooms = new GameObject[6];
+    public GameObject[] spawnpoints = new GameObject[6];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void Shuffle<T>(GameObject[] list, Random rnd)
+    {
+        for (int i = 0; i < list.Length; i++)
+            Swap(list,i, rnd.Next(i, list.Length));
+    }
+
+    public static void Swap<T>(GameObject[] list, int i, int j)
+    {
+        var temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+    }
+}
