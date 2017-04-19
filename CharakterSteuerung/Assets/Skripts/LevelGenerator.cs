ad75112317f5ec3b67fa05b7418bdbd03dcb8f5f
Jasmin <jasmin.leefers@web.de>
2017-04-19 16:41:16 +0200

    Mit ObjectSpawnern

--- CharakterSteuerung/Assets/Skripts/LevelGenerator.cs
+++ CharakterSteuerung/Assets/Skripts/LevelGenerator.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
     public GameObject[] rooms = new GameObject[6];
-    public GameObject[] spawnpoints = new GameObject[6];
-	// Use this for initialization
-	void Start () {
+    public GameObject[] roomspawnpoints = new GameObject[6];
+    public GameObject[] objects = new GameObject[4];
+    // Use this for initialization
+    void Start () {
         System.Random rnd = new System.Random();
         Shuffle(rooms, rnd);
-        Shuffle(spawnpoints, rnd);
-        Place();
-	}
+        Shuffle(roomspawnpoints, rnd);
+        Place(rooms, roomspawnpoints);
+
+        GameObject[] objectspawnpoints = new GameObject[6];
+        objectspawnpoints[0] = GameObject.Find("ObjectSpawn_Arbeitszimmer");
+        objectspawnpoints[1] = GameObject.Find("ObjectSpawn_Kueche");
+        objectspawnpoints[2] = GameObject.Find("ObjectSpawn_Wohnzimmer");
+        objectspawnpoints[3] = GameObject.Find("ObjectSpawn_Schlafzimmer");
+        objectspawnpoints[4] = GameObject.Find("ObjectSpawn_Kinderzimmer");
+        objectspawnpoints[5] = GameObject.Find("ObjectSpawn_Esszimmer");
+
+        Shuffle(objects, rnd);
+        Shuffle(objectspawnpoints, rnd);
+        Place(objects, objectspawnpoints);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    private void Place()
+    private void Place(GameObject[] objects, GameObject[] spawns)
     {
-        for(int s = 0; s< spawnpoints.Length; s++)
+        for(int s = 0; s < spawns.Length && s < objects.Length; s++)
         {
-            Instantiate(rooms[s], spawnpoints[s].transform.position, spawnpoints[s].transform.rotation);
+            Instantiate(objects[s], spawns[s].transform.position, spawns[s].transform.rotation);
         }
     }
 
