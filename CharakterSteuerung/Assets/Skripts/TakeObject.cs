f7828955482afa07126cc0f929f858623ed0df50
Jasmin <jasmin.leefers@web.de>
2017-04-21 15:37:07 +0200

    blubb

---
+++ CharakterSteuerung/Assets/Skripts/TakeObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))//("Action")
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Pickupable p = hit.collider.GetComponent<Pickupable>();
+                if(p != null)
+                {
+                    p.gameObject.SetActive(false);
+                    //Event einbauen, ->textausgabe
+                }
+            }
+        }
+
+    }
+}
