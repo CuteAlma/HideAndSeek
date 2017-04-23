8536ebc7912227fcd9a53f482961b85b7acac0bd
Ela <manuela.reker@gmx.net>
2017-04-23 17:04:33 +0200

    Alma Stops and Looks Around

--- CharakterSteuerung/Assets/Skripts/AI.cs
+++ CharakterSteuerung/Assets/Skripts/AI.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class AI : MonoBehaviour {
     public Transform target;
     public Transform eyes;
-    public float sight = 10f;
     public LayerMask playerLayer;
     public LayerMask obstacleLayer;
+
+    public float sight = 10f;
     public float chasingSpeed = 0.2f;
     public float reachedTarget = 2f;
-    Animator anim;
+    public float radius;
+    Vector3 destination;
+
     bool gameStart = false;
     bool gameOver = false;
+    bool wait = true;
+
+    Animator anim;
     AudioSource audio;
     public AudioClip gameOverSound;
-
+    private UnityEngine.AI.NavMeshAgent agent;
 
 
     void Start () {
+        destination = eyes.position;
         anim = GetComponent<Animator>();
         audio = GetComponent<AudioSource>();
+        agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     void Update () {
@@ -33,9 +41,20 @@ public class AI : MonoBehaviour {
         }
         
         RaycastHit hit;
-        if (gameOver == false)
+        if (gameOver == false && wait==false)
         {
-            
+
+            if (!agent.pathPending)
+            {
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                    {
+                        getNewDestination();
+                    }
+                }
+            }
+
             if (Physics.Raycast(eyes.position, direction, out hit, sight, obstacleLayer.value))
             {
                 Debug.DrawRay(eyes.position, direction * sight, Color.red);
@@ -49,7 +68,7 @@ public class AI : MonoBehaviour {
                     Debug.DrawRay(eyes.position, direction * sight, Color.green);
                     anim.SetFloat("gameOver", 0.2f);
                     Debug.logger.Log("GAME OVER");
-                    StartCoroutine("CoRoutineWaitForAnimationEnd");
+                    StartCoroutine("CoRoutineGameOver");
                     gameOver = true;
                     
                 }
@@ -68,12 +87,39 @@ public class AI : MonoBehaviour {
         
 	}
 
+    IEnumerator CoRoutineGameOver()
+    {
+        Debug.logger.Log("Begin GameOver");
+        yield return new WaitForSeconds(0.5f);
+        Debug.logger.Log("End GameOver");
+        Time.timeScale = 0;
+    }
+
     IEnumerator CoRoutineWaitForAnimationEnd()
     {
-        Debug.logger.Log("Begin");
-        yield return new WaitForSeconds(0.5f);
-        Debug.logger.Log("End");
-        Time.timeScale = 0;
+        Debug.logger.Log("Begin Animation");
+        yield return new WaitForSeconds(4.3f);
+        Debug.logger.Log("End Animation");
+        wait = false;
+        anim.SetFloat("searching", 0.00001f);
+    }
+
+    void getNewDestination()
+    {
+        radius = GetRandomNumber();
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        UnityEngine.AI.NavMeshHit hit;
+        UnityEngine.AI.NavMesh.SamplePosition(randomDirection, out hit, radius, 1);
+        destination = hit.position;
+
+        agent.SetDestination(destination);
+
+    }
+
+    public float GetRandomNumber()
+    {
+        return Random.Range(1.0f, 40.0f);
     }
 
 
@@ -81,8 +127,10 @@ public class AI : MonoBehaviour {
     {
         if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
         {
+            getNewDestination();
             anim.SetFloat("startGame", 1f);
             gameStart = true;
+            wait = false;
         }
         
     }
@@ -92,7 +140,11 @@ public class AI : MonoBehaviour {
         if(col.gameObject.layer == 10)
         {
             Debug.logger.Log("COLLISION DETECTED");
-            anim.SetFloat("gameOver", 0.2f);
+            agent.ResetPath();
+            wait = true;
+            anim.SetFloat("searching", 0.2f);
+            StartCoroutine("CoRoutineWaitForAnimationEnd");
+            
         }
     }
 
